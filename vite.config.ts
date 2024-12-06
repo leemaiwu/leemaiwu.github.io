8c047918c597cd5d9014d9d051bb7a0104d6e5c5
Laura Wu <leemaiwu@gmail.com>
2024-12-05 21:04:08 -0700

    reverted to original base

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/leemaiwu.github.io/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
